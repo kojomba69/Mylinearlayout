02f8b4f1fcf306ae39a4b17866c9f2bad1790847
kojomba69 <kojomba@naver.com>
2018-10-23 13:52:20 +0900

    Initial commit

---
+++ app/src/main/java/com/sunny/www/mylinearlayout/MainActivity.java
@@ -0,0 +1,35 @@
+package com.sunny.www.mylinearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void OnClick(View view){
+        switch (view.getId()){
+            case R.id.button1:
+                Toast.makeText(getApplicationContext(), "버튼1이 눌렸습니다.", Toast.LENGTH_SHORT).show(); break;
+            case R.id.button2:
+                Toast.makeText(getApplicationContext(), "버튼2이 눌렸습니다.", Toast.LENGTH_SHORT).show();break;
+            case R.id.button3:
+                Toast.makeText(getApplicationContext(), "버튼3이 눌렸습니다.", Toast.LENGTH_SHORT).show();break;
+        }
+
+    }
+    public void OnClick1(View view){
+        Toast.makeText(getApplicationContext(), "버튼1가 눌렸습니다.", Toast.LENGTH_SHORT).show();
+    }
+    public void OnClick2(View view){
+        Toast.makeText(getApplicationContext(), "버튼2가 눌렸습니다.", Toast.LENGTH_SHORT).show();
+    }
+    public void OnClick3(View view){
+        Toast.makeText(getApplicationContext(), "버튼3가 눌렸습니다.", Toast.LENGTH_SHORT).show();
+    }
+}
